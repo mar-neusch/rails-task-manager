e16cd67c1d2de15be06122b89422c5deabb1d44e
Marc Neuschwander <marc.neusch@icloud.com>
2020-11-17 15:19:41 +0100

    exercice done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # add a task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # list all task
+  get 'tasks', to: 'tasks#index'
+
+  # see details about a task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Update a restaurant
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
